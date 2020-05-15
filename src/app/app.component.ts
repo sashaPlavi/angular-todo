dca2a490ed1b60c01e7485a343084078f67b1573
SashaPlavi <sasha.bingulac@gmail.com>
2020-05-15 14:16:43 +0200

    all working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name: string = 'Sasha car';
+  name: string = "Sasha's todos";
 }
